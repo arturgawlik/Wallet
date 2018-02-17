67142e0254e6e6807f9644372615b9c70a00f841
Artur Gawlik <artur.gawlik@outlook.com>
2018-02-17 09:40:15 +0100

    base login screen

--- WalletWeb/Controllers/AccountController.cs
+++ WalletWeb/Controllers/AccountController.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WalletWeb.ViewModels;
 
 namespace WalletWeb.Controllers
 {
     public class AccountController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register(RegisterViewModel viewModel)
         {
             return View();
         }
--- WalletWeb/ViewModels/RegisterViewModel.cs
+++ WalletWeb/ViewModels/RegisterViewModel.cs
@@ -5,11 +5,12 @@ namespace WalletWeb.ViewModels
     public class RegisterViewModel
     {
         [Required, EmailAddress, MaxLength(256), Display(Name = "Email")]
-        public string Emial { get; set; }
+        public string Email { get; set; }
 
         [Required, MinLength(6), MaxLength(50), DataType(DataType.Password), Display(Name = "Password")]
         public string Password { get; set; }
 
+        [Required, MinLength(6), MaxLength(50), DataType(DataType.Password), Display(Name = "Password")]
         [Compare("Password", ErrorMessage = "The password does not match the confirmation password.")]
         public string ConfirmPassword { get; set; }
     }
